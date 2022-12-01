12e9d9b7d170f913cb7a93d708d85e796eeece61
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-12-02 00:45:20 +0330

    feat: complete root command

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"os"
 
+	"github.com/mehditeymorian/euler/internal/diagram"
+	"github.com/mehditeymorian/euler/internal/io"
 	"github.com/spf13/cobra"
 )
 
@@ -10,8 +12,11 @@ func Execute() {
 	rootCmd := &cobra.Command{
 		Use:   "euler",
 		Short: "euler graph generator",
-		Long:  "euler graph generator for golang struct models",
+		Long:  "euler graph generator for golang struct model",
+		Run:   run,
 	}
+
+	rootCmd.PersistentFlags().StringP("path", "p", "./", "directory of model")
 
 	err := rootCmd.Execute()
 	if err != nil {
@@ -19,4 +24,24 @@ func Execute() {
 	}
 }
 
+func run(cmd *cobra.Command, _ []string) {
+	path, _ := cmd.PersistentFlags().GetString("path")
 
+	files, err := io.Files(path)
+	if err != nil {
+		panic("failed to read files in the given directory")
+	}
+
+	structs, err := io.ExtractStructs(files)
+	if err != nil {
+		panic(err)
+	}
+
+	graph := diagram.CreateGraph(structs)
+
+	err = diagram.Render(graph, "out.svg")
+	if err != nil {
+		panic(err)
+	}
+
+}
