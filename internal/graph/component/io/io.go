94bca0ac65222a4d148b2a1877cbff261166aefc
Mehdi Teymorian <mehditeymorian322@gmail.com>
2023-11-08 14:54:01 +0330

    feat: add module name and component scanner

---
+++ internal/graph/component/io/io.go
@@ -0,0 +1,108 @@
+package io
+
+import (
+	"fmt"
+	"github.com/mehditeymorian/euler/internal/graph/component/model"
+	"go/parser"
+	"go/token"
+	"golang.org/x/mod/modfile"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func ScanComponents(path string, moduleName string) ([]model.Component, error) {
+	components := make(map[string]map[string]bool)
+
+	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			err := fmt.Errorf("failed to scan %s: %w", path, err)
+
+			return err
+		}
+
+		if d.IsDir() || !strings.HasSuffix(d.Name(), ".go") {
+			return nil
+		}
+
+		// Process Go source files
+		sourceCode, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return nil
+		}
+
+		fset := token.NewFileSet()
+		node, err := parser.ParseFile(fset, path, sourceCode, parser.ImportsOnly)
+		if err != nil {
+			fmt.Println("Error parsing file:", err)
+			return nil
+		}
+
+		packageName := node.Name.Name
+
+		_, ok := components[packageName]
+		if !ok {
+			components[packageName] = make(map[string]bool)
+		}
+
+		for _, spec := range node.Imports {
+			importPath, _ := strconv.Unquote(spec.Path.Value)
+			internalDependency := strings.HasPrefix(importPath, moduleName)
+			components[packageName][importPath] = internalDependency
+		}
+
+		return nil
+
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan files: %w", err)
+	}
+
+	var componentsArray []model.Component
+
+	for componentName, importsMap := range components {
+		var dependencies []model.Dependency
+
+		for importName, internal := range importsMap {
+			absoluteName := importName
+			if internal {
+				absoluteName = strings.TrimPrefix(absoluteName, moduleName)
+			}
+			dependencies = append(dependencies, model.Dependency{
+				Name:         importName,
+				AbsoluteName: absoluteName,
+				Internal:     internal,
+			})
+		}
+
+		componentsArray = append(componentsArray, model.Component{
+			Name:         componentName,
+			Dependencies: dependencies,
+		})
+	}
+
+	return componentsArray, nil
+}
+
+func ModuleName(path string) (string, error) {
+	// Read the content of the go.mod file
+	modFileContent, err := ioutil.ReadFile(path + "/go.mod")
+	if err != nil {
+		return "", fmt.Errorf("error reading go.mod file: %w", err)
+	}
+
+	// Parse the go.mod file content
+	modFile, err := modfile.Parse("go.mod", modFileContent, nil)
+	if err != nil {
+		return "", fmt.Errorf("error parsing go.mod file: %w", err)
+	}
+
+	// Extract the module name
+	moduleName := modFile.Module.Mod.Path
+
+	return moduleName, nil
+}
