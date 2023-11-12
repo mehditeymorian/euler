ad160cfadd25911c75239b2188893151c806cbba
Mehdi Teymorian <mehditeymorian322@gmail.com>
2023-11-12 18:46:43 +0330

    feat: glue everything together

--- cmd/component/component.go
+++ cmd/component/component.go
@@ -1,7 +1,9 @@
 package component
 
 import (
-	"fmt"
+	"github.com/mehditeymorian/euler/internal/graph/component/diagram"
+	"github.com/mehditeymorian/euler/internal/graph/component/io"
+	"github.com/mehditeymorian/euler/internal/graph/component/model"
 	"github.com/spf13/cobra"
 )
 
@@ -13,12 +15,36 @@ func Command() *cobra.Command {
 	}
 
 	cmd.PersistentFlags().StringP("path", "p", "./", "where to look for components")
+	cmd.PersistentFlags().StringP("output", "o", "out.svg", "output file name")
+	cmd.PersistentFlags().StringArrayP("exclude-dependencies", "d", nil, "exclude these dependencies from output")
+	cmd.PersistentFlags().StringArrayP("exclude-component", "c", nil, "exclude these components from output")
 
 	return cmd
 }
 
 func run(cmd *cobra.Command, _ []string) {
 	path, _ := cmd.PersistentFlags().GetString("path")
+	outputFileName, _ := cmd.PersistentFlags().GetString("output")
+	excludedDependencies, _ := cmd.PersistentFlags().GetStringArray("exclude-dependencies")
+	excludedComponents, _ := cmd.PersistentFlags().GetStringArray("exclude-components")
 
-	fmt.Println(path)
+	options := model.Option{
+		ExcludedDependencies: excludedDependencies,
+		ExcludedComponents:   excludedComponents,
+	}
+
+	moduleName, err := io.ModuleName(path)
+	if err != nil {
+		panic(err)
+	}
+
+	components, err := io.ScanComponents(path, moduleName, options)
+	if err != nil {
+		panic(err)
+	}
+
+	err = diagram.Render(components, moduleName, outputFileName)
+	if err != nil {
+		panic(err)
+	}
 }
